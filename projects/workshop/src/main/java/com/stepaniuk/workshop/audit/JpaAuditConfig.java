57fec0b426b410da8cc97cbb931d97167401eedc
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-09-23 21:52:26 +0300

    feat(audit): Added JPA Audit Config

---
+++ projects/workshop/src/main/java/com/stepaniuk/workshop/audit/JpaAuditConfig.java
@@ -0,0 +1,9 @@
+package com.stepaniuk.workshop.audit;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditConfig {
+}
