8cef69f97671726ae7d0a50f3bf946320a4218e0
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-10-02 12:44:55 +0300

    feat(feedback): Added and Implemented FeedbackRepository

---
+++ projects/workshop/src/main/java/com/stepaniuk/workshop/feedback/FeedbackRepository.java
@@ -0,0 +1,7 @@
+package com.stepaniuk.workshop.feedback;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface FeedbackRepository extends JpaRepository<Feedback, Long>, JpaSpecificationExecutor<Feedback> {
+}
