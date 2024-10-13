d2673cd65ba5a71753d9edde899462e8f0f76476
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-10-13 23:27:46 +0300

    feat(user): Added User Entity and repository for it

---
+++ projects/workshop/src/main/java/com/stepaniuk/workshop/user/User.java
@@ -0,0 +1,87 @@
+package com.stepaniuk.workshop.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_seq")
+    @SequenceGenerator(name = "users_id_seq", sequenceName = "users_id_seq", allocationSize = 1)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = true)
+    private String email;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private Instant createdAt;
+
+    @Column(name = "last_modified_at", nullable = false)
+    @LastModifiedDate
+    private Instant lastModifiedAt;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "id = " + id + ", " +
+                "firstName = " + firstName + ", " +
+                "lastName = " + lastName + ", " +
+                "email = " + email + ", " +
+                "phone = " + phone + ", " +
+                "createdAt = " + createdAt + ", " +
+                "lastModifiedAt = " + lastModifiedAt + ")";
+    }
+}
---
+++ projects/workshop/src/test/resources/sql/users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users(id, first_name, last_name, email, phone, password, created_at, last_modified_at)
+VALUES (1, 'First','Last','email@gmail.com','+380682019211','Password123!','2024-11-23T15:22:09.266615Z',
+        '2024-11-25T17:28:19.266615Z'),
+        (2, 'Second', 'Second', 'email@custom.com', '+380772228191', 'Pass!911', '2024-12-23T15:22:09.266615Z',
+         '2024-12-25T17:28:19.266615Z');
